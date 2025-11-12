c2c39cc8db9a757208cfad33147bc9f394a37c24
ErDoggi <155678572+ErDoggi@users.noreply.github.com>
2025-11-12 01:37:47 +0100

    -añadido archivo primerComp(PrimerComponente) -Reubicado archivo app.html -> /layouts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, forwardRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, forwardRef(() => primerComp)],
+  templateUrl: './layouts/app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('test');
+  protected readonly title = signal('Me cago en los muertos');
+}
+
+/**
+ * !----------------------------------------------------------------------------------
+ * Prueba de creacion del primer componente del demoino
+ * TODO: Lo que te salga de las pelotas
+ */
+
+@Component({
+  selector: 'primerComp',
+  imports: [RouterOutlet],
+  templateUrl: './layouts/primerComp.html',
+  styleUrl: './app.css'
+})
+export class primerComp{
+  nabo:string = "Esto es un nabo como una catedral";
 }

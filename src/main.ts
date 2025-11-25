10808719ec6053b521280502d1a36ba634df88a7
Raxabi <92801687+Raxabi@users.noreply.github.com>
2025-11-25 02:02:07 +0100

    feat: habilitar la ultra básica funcionalidad que en el grandioso y avanzado angular está desactivada :)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from '@app/app.routes';
 import { App } from '@app/app';
 
@@ -8,6 +8,6 @@ bootstrapApplication(App, {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: "enabled" }))
   ]
 }).catch((err) => console.error(err));
